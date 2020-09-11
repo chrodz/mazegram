8c361143ffc2464a055a84febcb59b233b34fc7b
Pierre Chavez <pierch.rod@gmail.com>
2020-09-11 18:12:50 -0500

    Estructurado mazegram

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,14 @@
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
+
 <div class="Footer">
     <div class="Footer-container">
-        <div class="Footer-copy">
-            <p>© 2020 MazeGram from Platzi</p>
-        </div>
+        <div class="Footer-copy">© 2020 MazeGram from Platzi</div>
     </div>
 </div>
